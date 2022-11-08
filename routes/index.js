9c03189208c08c5f3e281468ecaa2614fd827392
finnianmacken <finn@minerva.kgi.edu>
2022-11-08 18:07:22 -0300

    set up page creation routing

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var addPage = require('../functions/add-page-to-db');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
--- routes/page.js
+++ routes/page.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var addPage = require('../functions/add-page-to-db');
+var addPage = require('../functions/add-page');
 
-router.get('/create', async function(req, res, next) {
-  const pageResponse = await addPage("New! Bug with Whiteboard Loading Quickly")
-  console.log("PAGE RESPONSE:", pageResponse)
+router.post('/create', async function(req, res, next) {
+  console.log(req)
+  const body = req.body
+  console.log("BODY:", body)
+  // const pageResponse = await addPage("New! Bug with Whiteboard Loading Quickly")
+  const pageResponse = "hi"
   res.send(pageResponse);
 });
 
